cbe994fec43a27a1dc4f5660555e5df36b804e68
Carlos Herrera <herreradiaz14@gmail.com>
2025-04-10 22:12:35 -0500

    Commit clase de 10 de abril de 2025

--- CoinCapApp/app/src/main/java/com/example/coincapapp/MainActivity.kt
+++ CoinCapApp/app/src/main/java/com/example/coincapapp/MainActivity.kt
@@ -4,54 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.coincapapp.models.Asset
 import com.example.coincapapp.ui.theme.CoinCapAppTheme
-import com.example.coincapapp.views.AssetContainer
+import com.example.coincapapp.views.MainScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CoinCapAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column (
-                        verticalArrangement = Arrangement.Center,
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        AssetContainer(
-                            Asset(
-                                id = "1",
-                                name = "Bitcoin",
-                                symbol = "BTC",
-                                price = "$6525",
-                                percentage = 5.0
-                            )
-                        )
-                        Spacer(modifier = Modifier.size(16.dp))
-                        AssetContainer(
-                            Asset(
-                                id = "2",
-                                name = "Tether",
-                                symbol = "USDT",
-                                price = "$100",
-                                percentage = -15.0
-                            )
-                        )
-                    }
-                }
+                MainScreen()
             }
         }
     }
